8968e7c5b5fa7b850c647f3f8d795daf33fd7c4c
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2021-11-14 21:36:50 -0800

    Tentative attempt to install ray using their dockerfile settings

--- dockerfiles/ray/run_image.fish
+++ dockerfiles/ray/run_image.fish
@@ -5,12 +5,11 @@
 # the 6379 port is for redis
 # if you use this container as the head of a ray node the other nodes need to be able to access its redis server
 
-docker run -p 2225:22 --name bowling-ray \
+docker run -p 2225:22 -p 6379:6379 --name bowling-ray \
+       --shm-size=9.18gb \
        --mount type=bind,source=$HOME/projects/Bowling-For-Data-Private,target=/home/bravo/Bowling-For-Data-Private \
        --mount type=bind,source=$HOME/projects/Bowling-For-Data,target=/home/bravo/Bowling-For-Data \
        --mount type=bind,source=/media/data,target=/home/bravo/data \
        --mount type=bind,source=$HOME/projects/graeae,target=/home/bravo/graeae \
        --mount type=bind,source=$HOME/projects/models/,target=/home/bravo/models \
-       --shm-size=9.18gb \
-       -p 6379:6379 \
        -it bowling-ray
