507a885c7cde99fb3d201c1a683bd6cd0d91a638
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2021-11-15 17:30:56 -0800

    Updated gitignore and dockerfiles for pypy
    Added .gitignore references to dask and pyenv files/folders
    Added expects and ptpython to requirements.txt
    Added mounting of private run-image in docker container

--- dockerfiles/pypy/run_image.fish
+++ dockerfiles/pypy/run_image.fish
@@ -1,6 +1,7 @@
 #! /usr/bin/env fish
 docker run -p 2223:22 --name bowling-pypy \
        --mount type=bind,source=$HOME/projects/Bowling-For-Data,target=/home/bravo/Bowling-For-Data \
+       --mount type=bind,source=$HOME/projects/Bowling-For-Data-Private,target=/home/bravo/Bowling-For-Data-Private \
        --mount type=bind,source=/media/data,target=/home/bravo/data \
        --mount type=bind,source=$HOME/projects/graeae,target=/home/bravo/graeae \
        --mount type=bind,source=$HOME/projects/models/,target=/home/bravo/models \
