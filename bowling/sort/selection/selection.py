e8599630b68039be3648f85887f87e24f69d3571
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2021-11-26 14:42:00 -0700

    Adding Selection Sort Swap Plotting
     - Adding plot comparing bubble sort and selection sort
     - Added plot showing how list changes with each swap in selection sort
     - Extended the Bubble Sort runtime section to explain where the math is
       coming from
     - Added citation post for How To Think About Algorithms
     - Added citation post for article on Bubble Sort history

---
+++ bowling/sort/selection/selection.py
@@ -0,0 +1,60 @@
+from collections.abc import MutableSequence
+from typing import Any, Dict
+Counts = tuple[int, int, int]
+Swaps = Dict[int, list[int]]
+Sortable = MutableSequence[Any]
+
+def selection_counter(elements: Sortable) -> Counts:
+    """Does the selection sort on the elements
+
+    Args:
+     elements: list of orderable objects
+
+    Returns:
+     (number of elements, comparisons, swaps)
+    """
+    number_of_elements = len(elements)
+    comparisons = swaps = 0
+    
+    for start_of_unselected in range(number_of_elements - 1):
+        smallest_unselected = start_of_unselected
+        for next_unselected in range(start_of_unselected + 1,
+                                     number_of_elements):
+            comparisons += 1
+            if elements[next_unselected] < elements[smallest_unselected]:
+                smallest_unselected = next_unselected
+        swaps += 1
+        elements[start_of_unselected], elements[smallest_unselected] = (
+            elements[smallest_unselected], elements[start_of_unselected]
+        )
+    return (number_of_elements, comparisons, swaps)
+
+def selection_swaps(elements: Sortable) -> Swaps:
+    """Keeps track of the element indexes as they are swapped
+
+    Args:
+     elements: list of orderable elements
+
+    Returns:
+     dict mapping element to list of indices where it was in the elements list
+    """
+    swaps = {element: [index] for index, element in enumerate(elements)}
+
+    number_of_elements = len(elements)
+
+    for start_of_unselected in range(number_of_elements - 1):
+        smallest_unselected = start_of_unselected
+
+        for next_unselected in range(start_of_unselected + 1,
+                                     number_of_elements):
+            if elements[next_unselected] < elements[smallest_unselected]:
+                smallest_unselected = next_unselected
+        
+        elements[start_of_unselected], elements[smallest_unselected] = (
+            elements[smallest_unselected], elements[start_of_unselected]
+        )
+
+        # record the location of the elements
+        for index, element in enumerate(elements):
+            swaps[element].append(index)
+    return swaps
