6c7d63a420ec9cea16b7f456a415594e16475426
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2022-03-14 18:38:17 -0700

    Added BST Query post

---
+++ bowling/data_structures/binary_search_tree/tree.py
@@ -0,0 +1,36 @@
+# pypi
+from attrs import define
+
+# this project
+from .node import Node
+
+
+@define
+class Tree:
+    """Binary Search Tree
+
+    Args:
+     root: the root node for the tree
+    """
+    root: Node = None
+
+    def insert(self, node: Node) -> None:
+        """Insert the node as a new leaf in the tree
+    
+        Args:
+         node: a node to insert into the tree
+        """
+        hunter, hound = None, self.root
+    
+        while hound is not None:
+            hunter, hound = hound, hound.left if node < hound else hound.right
+    
+        node.parent = hunter
+    
+        if hunter is None:
+            self.root = node
+        elif node.key < hunter.key:
+            hunter.left = node
+        else:
+            hunter.right = node
+        return
